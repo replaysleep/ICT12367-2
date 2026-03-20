5ba8b870d870e21e8b82df55c280d10d7dcc7b84
REPLAY <replayy999@gmail.com>
2026-03-20 16:01:04 +0700

    Add files via upload

---
+++ Lab10/myapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from myapp.models import Person
+
+# Create your views here.
+def index(request):
+    all_person = Person.objects.all()
+    return render(request, 'index.html', {"all_person": all_person})
+
+def about(request):
+    return render(request, 'about.html')
+
+def form(request):
+    return render(request, 'form.html')
+    
